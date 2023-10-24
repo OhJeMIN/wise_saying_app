93bee34056696029643f9891330a69eb7a3e65aa
jasonoh22 <jasonoh22@naver.com>
2023-10-24 13:48:53 +0900

    1단계수정

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,12 @@
 package com.ll;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("== 명언 앱 ==");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명령)");
+        String str = scanner.nextLine();
     }
 }
