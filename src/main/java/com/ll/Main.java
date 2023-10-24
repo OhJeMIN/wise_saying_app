50e846ad403f5845fc4373f2081f24363e3e6cd5
jasonoh22 <jasonoh22@naver.com>
2023-10-24 14:10:16 +0900

    2단계 [등록]

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,7 +6,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("명령)");
+        System.out.print("명령) ");
         String str = scanner.nextLine();
+        int sw_num = 0;
+        while(!str.equals("종료")){
+            if(str.equals("등록")) {
+                sw_num ++;
+                System.out.print("명언 : ");
+                String saying_wise = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+                System.out.println(sw_num+"번 명언이 등록되었습니다.");
+                System.out.print("명령) ");
+                str = scanner.nextLine();
+            }
+        }
+
     }
 }
